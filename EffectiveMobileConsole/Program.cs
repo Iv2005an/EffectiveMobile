6c6ecfdd28327e658fc2e10e1801ed5cb947c2d1
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-10-27 19:14:20 +0300

    feat: add main script

--- EffectiveMobileConsole/Program.cs
+++ EffectiveMobileConsole/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EffectiveMobileConsole.Models;
+using EffectiveMobileConsole.Services;
+
+try
+{
+    ArgsParser parser = new(args);
+    List<Order> orders = OrderService.ReadOrders(@"orders.csv");
+    orders = OrderService.FilterOrders(orders, parser.CityDistrict, parser.FirstDeliveryDateTime);
+    OrderService.WriteOrders(orders, parser.DeliveryOrder ?? "");
+}
+catch (Exception ex)
+{
+	try
+	{
+        LogService.Log(new() { Level = LogEvent.LogLevels.Error, Message = ex.Message });
+    }
+    catch (Exception logEx)
+	{
+        PrintService.PrintErrorMessage($"LOG ERROR: {logEx.Message}");
+	}
+}
+
