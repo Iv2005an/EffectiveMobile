7155e7ec26d9bf504111fb9491462aca0ea7851a
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-10-27 11:04:35 +0300

    feat: add print service

---
+++ EffectiveMobileConsole/Services/PrintService.cs
@@ -0,0 +1,17 @@
+﻿namespace EffectiveMobileConsole.Services;
+
+internal static class PrintService
+{
+    static void PrintMessage(string message, ConsoleColor foregroundColor)
+    {
+        ConsoleColor baseColor = Console.ForegroundColor;
+        Console.ForegroundColor = foregroundColor;
+        Console.Write(message);
+        Console.ForegroundColor = baseColor;
+    }
+    public static void PrintInfoMessage(string message) => PrintMessage(message, ConsoleColor.Blue);
+    public static void PrintSuccessMessage(string message) => PrintMessage(message, ConsoleColor.Green);
+    public static void PrintWarningMessage(string message) => PrintMessage(message, ConsoleColor.Yellow);
+    public static void PrintErrorMessage(string message) => PrintMessage(message, ConsoleColor.Red);
+
+}
