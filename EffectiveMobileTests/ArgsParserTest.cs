0722d108b042bd61a3381954b0f6d5e7c889ca45
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-10-27 19:45:42 +0300

    feat: add tests for args parser

--- EffectiveMobileTests/ArgsParserTest.cs
+++ EffectiveMobileTests/ArgsParserTest.cs
@@ -22,6 +22,35 @@ public class ArgsParserTest
         Assert.Equal(deliveryOrder, parser.DeliveryOrder);
     }
     [Fact]
+    public void TestParsing_ValidArguments1()
+    {
+        string cityDistrict = "1";
+        string firstDeliveryDateTime = "2024-10-25 14:34:23";
+        string deliveryLog = @"..";
+        string deliveryOrder = "C:\\";
+        ArgsParser parser = new([
+            "_cityDistrict", cityDistrict,
+            "_firstDeliveryDateTime", firstDeliveryDateTime,
+            "_deliveryLog", deliveryLog,
+            "_deliveryOrder", deliveryOrder,]);
+        Assert.Equal(cityDistrict, parser.CityDistrict.ToString());
+        Assert.Equal(firstDeliveryDateTime, parser.FirstDeliveryDateTime.ToString("yyyy-MM-dd HH:mm:ss"));
+        Assert.Equal(Path.GetFullPath(deliveryLog), parser.DeliveryLog);
+        Assert.Equal(deliveryOrder, parser.DeliveryOrder);
+    }
+    [Fact]
+    public void TestParsing_SkipRequiredArgument() =>
+    Assert.Throws<Exception>(() =>
+    {
+        string cityDistrict = "1";
+        string deliveryLog = "C:\\";
+        string deliveryOrder = "C:\\";
+        ArgsParser parser = new([
+            "_cityDistrict", cityDistrict,
+            "_deliveryLog", deliveryLog,
+            "_deliveryOrder", deliveryOrder,]);
+    });
+    [Fact]
     public void TestParsing_InvalidÑityDistrict() =>
     Assert.Throws<Exception>(() =>
     {
