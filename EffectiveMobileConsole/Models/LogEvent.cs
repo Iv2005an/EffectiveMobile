44472a1bbc2e58791bb4181f24214e9a9e897c11
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-10-27 19:12:35 +0300

    feat: add service for logging

---
+++ EffectiveMobileConsole/Models/LogEvent.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace EffectiveMobileConsole.Models;
+
+public class LogEvent
+{
+    public enum LogLevels
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    [Format("yyyy-MM-dd HH:mm:ss")]
+    public DateTime LogDateTime { get; set; } = DateTime.Now;
+
+    public required LogLevels Level { get; set; }
+
+    public required string Message { get; set; }
+}
---
+++ EffectiveMobileConsole/Services/LogService.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using EffectiveMobileConsole.Models;
+using System.Globalization;
+
+namespace EffectiveMobileConsole.Services;
+
+public static class LogService
+{
+    const string logFile = "logs.csv";
+    static string logPath = "";
+
+    public static string LogPath
+    {
+        get => Path.GetFullPath(Path.Combine(logPath, logFile));
+        set
+        {
+            string path = Path.GetFullPath(value);
+            if (File.Exists(path)) logPath = path;
+            else throw new Exception($"Invalid path {path}");
+        }
+    }
+
+    public static void Log(LogEvent logEvent)
+    {
+        switch (logEvent.Level)
+        {
+            case LogEvent.LogLevels.Info:
+                PrintService.PrintInfoMessage(logEvent.Message);
+                break;
+            case LogEvent.LogLevels.Warning:
+                PrintService.PrintWarningMessage(logEvent.Message);
+                break;
+            case LogEvent.LogLevels.Error:
+                PrintService.PrintErrorMessage(logEvent.Message);
+                break;
+        }
+        bool isAppend = File.Exists(LogPath);
+        using StreamWriter writer = new(LogPath, isAppend);
+        using CsvWriter csv = new(writer, CultureInfo.InvariantCulture);
+        if (!isAppend)
+        {
+            csv.WriteHeader<LogEvent>();
+            csv.NextRecord();
+        }
+        csv.WriteRecord(logEvent);
+        csv.NextRecord();
+    }
+}
